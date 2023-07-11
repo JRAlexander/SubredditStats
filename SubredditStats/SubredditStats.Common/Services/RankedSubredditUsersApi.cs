97f73cde1f2823ef40d14a48c5211513683ff0e6
John Alexander <4804801+JRAlexander@users.noreply.github.com>
2023-07-11 01:51:59 -0500

    initial files

---
+++ SubredditStats/SubredditStats.Common/Services/RankedSubredditUsersApi.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using SubredditStats.Common.Models;
+
+namespace SubredditStats.Common.Services
+{
+    public static class RankedSubredditUsersApi
+    {
+        public static async Task<IResult> GetRankedSubredditUsers(RankedSubredditUserDb rankedSubredditUserDb)
+        {
+            return TypedResults.Ok(await rankedSubredditUserDb.RankedSubredditUsers.Select(u => new { u.Name, u.totalPosts }).OrderByDescending(u => u.totalPosts).ToListAsync());
+        }
+    }
+}
+
